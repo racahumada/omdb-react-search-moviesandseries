ad6d73c88463692d1f8a6caf68ea50dc9b5269ee
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2020-09-26 18:24:33 -0300

    SearchPainel Ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 // import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import SearchPainel from './components/SearchPainel';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [dataSearch, setDataSearch] = useState([]);
+  const searchAndApi = async (textSearch, typeSearch) => {
+    console.log(textSearch, typeSearch);
+  };
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={2}>
+          <SearchPainel submitSearch={searchAndApi} />
+        </Col>
+        <Col xs={12} md={10}>
+          teste 2
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/SearchPainel.js
@@ -0,0 +1,88 @@
+import React, { Fragment } from 'react';
+import {
+  FormControl,
+  Row,
+  Col,
+  ButtonGroup,
+  Button,
+  ToggleButton,
+} from 'react-bootstrap';
+import { useState } from 'react';
+
+function SearchPainel(props) {
+  const { submitSearch } = props;
+
+  const [radioType, setRadioType] = useState('');
+  const [inputSearch, setInputSearch] = useState('');
+
+  const arrBtnRadio = [
+    { name: 'Filme', value: 'movie' },
+    { name: 'Série', value: 'series' },
+  ];
+
+  const handleCheck = (e) => {
+    const { value } = e.target;
+    setRadioType(value);
+  };
+
+  const handleText = (e) => {
+    const { value } = e.target;
+    setInputSearch(value);
+  };
+
+  const clickSubmit = () => {
+    submitSearch(inputSearch, radioType);
+  };
+  return (
+    <Fragment>
+      <Row>
+        <Col>
+          <h6 className="mt-3">PESQUISAR FILME OU SÉRIE:</h6>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex flex-column">
+          <label>Filme ou Série?</label>
+          <ButtonGroup toggle>
+            {arrBtnRadio.map((btnRadio, id) => {
+              const { name, value } = btnRadio;
+              return (
+                <ToggleButton
+                  key={id}
+                  type="radio"
+                  variant="outline-primary"
+                  name="type"
+                  value={value}
+                  checked={radioType === value}
+                  onChange={handleCheck}
+                  className="mb-3"
+                >
+                  {name}
+                </ToggleButton>
+              );
+            })}
+          </ButtonGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <FormControl
+            variant="outline-primary"
+            placeholder="Digite aqui..."
+            onChange={handleText}
+            className="mb-3 border-primary"
+          ></FormControl>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={clickSubmit} block>
+            Pesquisar
+          </Button>
+        </Col>
+      </Row>
+    </Fragment>
+  );
+}
+
+export default SearchPainel;
